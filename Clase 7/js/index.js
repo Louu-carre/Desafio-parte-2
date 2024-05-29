e3ad8514f7938df865dfc7b0811854c321daca66
Lou Carre <lourdescarregarcia@gmail.com>
2024-05-29 17:27:21 -0300

    Clase 7

---
+++ Clase 7/js/index.js
@@ -0,0 +1,7 @@
+setTimeout(()=> {
+let precioProducto = document.getElementById ('precioProducto');
+let titulo = document.querySelector ('#nombreProducto')
+
+precioProducto.innerText = 'Precio: $450'
+titulo.style.color = 'pink'
+},2000)
